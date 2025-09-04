e1ad8a5fa1c0faaf14c78808c4465315ee945580
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-04 18:20:29 -0300

    first commit

---
+++ src/services/BrazilInterestService.java
@@ -0,0 +1,14 @@
+package services;
+
+public class BrazilInterestService implements InterestService {
+	private Double interestRate;
+
+	public BrazilInterestService(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+}
